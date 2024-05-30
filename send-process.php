506a8ea50e0504923f7966762b4452db8e3dc68c
dezsk <128952034+dezsk@users.noreply.github.com>
2024-05-30 14:06:03 +0700

    Add files via upload

---
+++ send-process.php
@@ -0,0 +1,67 @@
+<?php
+    $fops =fopen("pesanan.txt", "a++");
+    $namaUser = $_POST['nama'];
+    $emailUser = $_POST['telp'];
+    $alamatUser = $_POST['address'];
+    $servicesUser = $_POST['services'];
+    $complaintUser = $_POST['complaint'];
+
+    fputs($fops, "$namaUser | $emailUser | $alamatUser | $servicesUser | $complaintUser | \n");
+    fclose($fops);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pesan Terkirim</title>
+</head>
+<style type="text/css">
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+    border: none;
+    text-decoration: none;
+    }
+    .main {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+        background-image: url('image-resource/mainbg.jpg');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        position: relative;
+        justify-content: center;
+        color: white;
+        font-size: 2rem;
+    } 
+    .main .message {
+        padding-right: 2rem;
+    }
+    .main a {
+        margin-top: 1rem;
+        padding: 1rem 3rem;
+        font-size: 2rem;
+        display: inline-block;
+        background-color: #54B435;
+        border-radius: 1rem;
+        color: #fff;
+        box-shadow: 1px 1px 3px rgba(1, 1, 3, 0.5);
+        }
+
+</style>
+<body>
+    <section class="main">
+        <div class="message">
+            <h2>Pesanan anda telah terkirim kepada <span>kami</span><br>TERIMA KASIH</h2>
+        </div>
+        <div class="readmessage">
+            <a href="send-success.php">Lihat pesan</a>
+        </div>
+    </section>
+</body>
+</html>
